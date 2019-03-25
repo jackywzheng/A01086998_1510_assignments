01b9fcbeef3b6733b69c70fc052feaa3d65f694b
Jacky Zheng <30736593+jackywzheng@users.noreply.github.com>
2019-03-24 23:59:14 -0700

    More functions added.

--- A4/crud.py
+++ A4/crud.py
@@ -6,12 +6,11 @@
 
 
 class Student:
-    def __init__(self, first_name: str, last_name: str, student_number: str, status: str, final_grades: list):
+    def __init__(self, first_name: str, last_name: str, student_number: str, status: str):
         self.first_name = first_name
         self.last_name = last_name
         self.student_number = student_number
         self.status = status
-        self.final_grades = final_grades
 
     def get_first_name(self):
         return self.first_name
@@ -25,28 +24,44 @@ class Student:
     def get_status(self):
         return self.status
 
-    def get_final_grades(self):
-        return self.final_grades
-
     def print_info(self):
         print("Name:", self.first_name, self.last_name, "Student Number: ", self.student_number,
-              "Status: ", self.status, "Final grades: ", self.final_grades)
+              "Status: ", self.status)
 
 
 def add_student():
     first_name = input("Enter the student's first name: ")
     last_name = input("Enter the student's last name: ")
     student_number = input("Enter the student number: ")
+    status = input("Enter the student's status (in good standing or not in good standing): ")
+    try:
+        new_student = Student(first_name, last_name, student_number, status)
+        file_write(new_student)
+    except TypeError:
+        print("Error. You did not enter the required information to add a new student. Returning to menu.")
+
+
+def file_delete_student(delete_student_number):
+    with open("students.txt", "r+") as file_object:
+        student_file = file_object.readlines()
+        file_object.seek(0)
+        for line in student_file:
+            if line != delete_student_number:
+                file_object.write(line)
+                return True
+            else:
+                return False
 
 
 def file_read():
     with open("students.txt") as file_object:
-        file_object.read()
+        file_object.readlines()
 
 
-def file_write():
-    with open("students.txt", 'w') as file_object:
-        file_object.read()
+def file_write(new_student):
+    with open("students.txt", 'a') as file_object:
+        file_object.write(new_student.first_name + " " + new_student.last_name + " " +
+                          new_student.student_number + " " + new_student.status + "\n")
 
 
 def main():
@@ -56,7 +71,12 @@ def main():
         if choice == 1:
             pass
         elif choice == 2:
-            pass
+            delete_student_number = input("What is the student number that you wish to delete? ")
+            delete_result = file_delete_student(delete_student_number)
+            if delete_result is True:
+                print("The student has been deleted.")
+            else:
+                print("Student number was not found. The student was not deleted.")
         elif choice == 3:
             pass
         elif choice == 4:
